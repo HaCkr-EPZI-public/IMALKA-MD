abd2793ca7a5d152a2e46099553dfec48a3905cf
cobr23 <contactprasad32@gmail.com>
2024-09-27 11:20:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,4 @@ SESSION_ID: process.env.SESSION_ID || "3BozDJxD#w2EN81MMVpCHnQsYLczxXPBd6f1D-3uh
 MONGODB: process.env.MONGODB || "url",
 PEXELS_API_KEY: process.env.PEXELS_API_KEY || "39WCzaHAX939xiH22NCddGGvzp7cgbu1VVjeYUaZXyHUaWlL1LFcVFxH",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
